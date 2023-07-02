7c54f2e3dc4a2f9a77e6d85a54ca6a722d34b26a
Antonio Dias <antonio.dias@neogrid.com>
2023-07-01 23:49:50 -0300

    Add docker compose to MongoDB and first commit from Java Api

---
+++ api-sport-news/src/main/java/com/sportnews/api/SportNewsApplication.java
@@ -0,0 +1,13 @@
+package com.sportnews.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportNewsApplication.class, args);
+	}
+
+}
