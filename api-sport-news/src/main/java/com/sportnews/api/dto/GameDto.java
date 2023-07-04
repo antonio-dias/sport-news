34a053af7f308ba03521efdd1dec0b2a7dce401a
Antonio Dias <antonio.dias@neogrid.com>
2023-07-03 23:25:56 -0300

    Adjusting game registration endpoint

--- api-sport-news/src/main/java/com/sportnews/api/dto/GameDto.java
+++ api-sport-news/src/main/java/com/sportnews/api/dto/GameDto.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
 
 @Data
-@Document
+@Document(value = "games")
 public class GameDto {
 	
 	@Id
--- api-sport-news/src/main/java/com/sportnews/api/dto/Summary.java
+++ api-sport-news/src/main/java/com/sportnews/api/dto/Summary.java
@@ -1,5 +1,8 @@
 package com.sportnews.api.dto;
 
+import lombok.Data;
+
+@Data
 public class Summary {
 	
 	private String homeTeam;
